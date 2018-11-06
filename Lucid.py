ed1ca511e1220e7b873612db8489ce76cb046300
David Kelemen <kdawid93@gmail.com>
2018-11-06 22:29:28 +0100

    Iterating throw one layer to show all pictures

--- Lucid.py
+++ Lucid.py
@@ -8,15 +8,22 @@ import lucid.optvis.param as param
 import lucid.optvis.render as render
 import lucid.optvis.transform as transform
 
+import matplotlib.pyplot as plt
+
+def printNeurons(data):
+    for i in range(len(data)):
+        for j in range(len(data[i])):
+            plt.imshow(data[i][j])
+            plt.show()
 
 model = models.InceptionV1()
 model.load_graphdef()
 
-from keras.layers import Dense
-from keras.layers import GlobalAveragePooling2D
-
-model.show_graph()
-
 data = render.render_vis(model, "mixed4a_pre_relu:476")
+printNeurons(data)
+
+for i in range(450,460):
+    data = render.render_vis(model, "mixed4a_pre_relu:%s" % i)
+    printNeurons(data)
 
 print("end.")
